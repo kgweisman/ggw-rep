398215f3ac0b2b8507a421908b9e4f81a25bf0c7
kgweisman <kweisman@stanford.edu>
2015-02-09 18:25:37 -0800

    add long's helper functions to js script

---
+++ experiment/experiment.js
@@ -0,0 +1,14 @@
+// set up helper functions from long
+
+function showSlide(id) {
+	$(".slide").hide(); // hide all slides
+	$("#"+id).show(); // show selected slide
+};
+
+function randomInteger(n) { 
+	return Math.floor(Math.random()*n); // get a random integer < n
+}
+
+function randomElement(array) {
+	return array[randomInteger(array.length)]; // get a random element out of an array
+}
