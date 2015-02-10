d43285a050d451df8c7d570f2566e6af7eb4e3c2
kgweisman <kweisman@stanford.edu>
2015-02-09 19:02:29 -0800

    add plan of attack in comments

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -14,5 +14,27 @@ function randomElement(array) {
 }
 
 /* show consent slide */
+
 showSlide("consent");
 
+/* when user clicks "Next" button, advance to "instructions" slide */
+
+/* when user clicks "Next" button, advance to "characters" slide */
+
+/* when user clicks "Next" button, advance to "surveys" slide */
+
+/* when user clicks "Start ___ Survey" button, advance to stage slide */
+
+	/* fill in stage slide with appropriate question prompt based on survey selected above */
+
+	/* fill in stage slide with randomized order of character pairs and randomized left/right position */
+
+	/* advance through 78 trials */
+
+/* when user finishes 78 trials, advance to "demographics" slide */
+
+/* when user clicks "Next" button, advance to "results" slide */
+
+/* when user clicks "Next" button, advance to "finished" slide */
+
+/* submit data via mmturkey */
