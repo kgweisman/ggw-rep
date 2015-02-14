7ec8dd333291423414fd477d4f423ac48932c4a0
kgweisman <kweisman@stanford.edu>
2015-02-13 19:36:04 -0800

    make createCharacters function to make characters with names and image sources

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -13,7 +13,57 @@ function randomElement(array) {
 	return array[randomInteger(array.length)]; // get a random element out of an array
 }
 
+/* set up list of characters with image sources */
+function createCharacters() {
+	function Character(charName) {
+		this.charName = charName;
+		this.imageSource = "images_characters/"+charName+".png";
+	}
+	var charlie = new Character("charlie_dog");
+	var delores = new Character("delores-gleitman_deceased");
+	var fetus = new Character("fetus");
+	var gerald = new Character("gerald-schiff_pvs");
+	var god = new Character("god");
+	var greenfrog = new Character("green-frog");
+	var kismet = new Character("kismet_robot");
+	var nicholas = new Character("nicholas-gannon_baby");
+	var samantha = new Character("samantha-hill_girl");
+	var sharon = new Character("sharon-harvey_woman");
+	var toby = new Character("toby_chimp");
+	var todd = new Character("todd-billingsley_man");
+	var you = new Character("you");
+	var listChars = [charlie, delores, fetus, gerald, 
+					god, greenfrog, kismet, nicholas,
+					samantha, sharon, toby, todd, you];
+	console.log(listChars[0]);
+}
+
+createCharacters();
+
+/* here's a version where I created the characters as global variables... */
+// function Character(charName) {
+// 	this.charName = charName;
+// 	this.imageSource = "images_characters/"+charName+".png";
+// }
+
+// var charlie = new Character("charlie_dog");
+// var delores = new Character("delores-gleitman_deceased");
+// var fetus = new Character("fetus");
+// var gerald = new Character("gerald-schiff_pvs");
+// var god = new Character("god");
+// var greenfrog = new Character("green-frog");
+// var kismet = new Character("kismet_robot");
+// var nicholas = new Character("nicholas-gannon_baby");
+// var samantha = new Character("samantha-hill_girl");
+// var sharon = new Character("sharon-harvey_woman");
+// var toby = new Character("toby_chimp");
+// var todd = new Character("todd-billingsley_man");
+// var you = new Character("you");
+// 
+// console.log(charlie.imageSource);
+
 /* set up structure of experiment */
+
 var experiment = {
 	parts: ["consent", "instructions", "characters", "surveys", "stage", "results", "finished"],
 	condition: "none",
