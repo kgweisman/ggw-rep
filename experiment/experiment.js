13b5dd76570db8f2edf9bb6f6b75385bda819762
kgweisman <kweisman@stanford.edu>
2015-02-13 16:58:35 -0800

    set up showSlide for all slide types

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -15,15 +15,23 @@ function randomElement(array) {
 
 /* show consent slide */
 
-// showSlide("consent");
+showSlide("consent");
 
 /* when user clicks "Next" button, advance to "instructions" slide */
 
+showSlide("instructions");
+
 /* when user clicks "Next" button, advance to "characters" slide */
+
+showSlide("characters");
 
 /* when user clicks "Next" button, advance to "surveys" slide */
 
+showSlide("surveys");
+
 /* when user clicks "Start ___ Survey" button, advance to "stage" slide */
+
+showSlide("stage");
 
 	/* fill in stage slide with appropriate question prompt based on survey selected above */
 
@@ -37,6 +45,10 @@ showSlide("demographics");
 
 /* when user clicks "Next" button, advance to "results" slide */
 
+showSlide("results");
+
 /* when user clicks "Next" button, advance to "finished" slide */
+
+showSlide("finished");
 
 /* submit data via mmturkey */
