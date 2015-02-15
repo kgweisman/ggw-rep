cd49e673669ad0e0ea862e29a3deeebf48c8c1b0
kgweisman <kweisman@stanford.edu>
2015-02-15 13:38:49 -0800

    set up some display properties of images

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -20,25 +20,26 @@ function randomElementNR(bucket) { // select without replacement
 
 /* set up list of characters with image sources */ 
 
-function Character(charName, charDescrip) {
+function Character(charName, charTitle, charDescrip) {
 	this.charName = charName;
+	this.charTitle = charTitle;
 	this.imageSource = "images_characters/"+charName+".png";
 	this.charDescrip = charDescrip;
 }
 
-var charlie = new Character("charlie_dog", "Charlie is a 3-year-old Springer spaniel and a beloved member of the Graham family.");
-var delores = new Character("delores-gleitman_deceased", "Delores Gleitman recently passed away at the age of 65. As you complete the survey, please draw upon your own personal beliefs about people who have passed away.");
-var fetus = new Character("fetus", "At 7 weeks, a human fetus is almost half an inch long - roughly the size of a raspberry.");
-var gerald = new Character("gerald-schiff_pvs", "Gerald Schiff has been	in a persistent vegetative state (PVS) for the past six months. Although he has severe brain damage - Gerald does not appear to communicate with others or make purposeful movements - his basic bodily functions (such as breathing, sleeping, and circulation) are preserved.");
-var god = new Character("god", "Many people believe that God is the creator of the universe and the ultimate source of knowledge, power, and love. However, please draw upon your own personal beliefs about God.");
-var greenfrog = new Character("green-frog", "The Green Frog can be found throughout eastern North America. This classic 'pond frog' is medium-sized and green or bronze in color. Daily life includes seeking out permanent ponds or slow streams with plenty of vegetation.");
-var kismet = new Character("kismet_robot", "Kismet is part of a new class of 'sociable' robots that can engage people in natural interaction. To do this, Kismet perceives a variety of natural social signals from sound and sight, and delivers his own signals back to the human partner through gaze direction, facial expression, body posture, and vocal babbles.");
-var nicholas = new Character("nicholas-gannon_baby", "Nicholas is a five-month-old baby.");
-var samantha = new Character("samantha-hill_girl", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
-var sharon = new Character("sharon-harvey_woman", "Sharon Harvey, 38, works at an advertising agency in Chicago.");
-var toby = new Character("toby_chimp", "Toby is a two-year-old wild chimpanzee living at an outdoor laboratory in Uganda.");
-var todd = new Character("todd-billingsley_man", "Todd Billingsly is a thirty-year-old accountant who lives in New York City.");
-var you = new Character("you", "When you see the mirror, please consider how you, yourself, would compare with the other choice presented.");
+var charlie = new Character("charlie_dog", "Charlie, Family Dog", "Charlie is a 3-year-old Springer spaniel and a beloved member of the Graham family.");
+var delores = new Character("delores-gleitman_deceased", "Delores Gleitman, Recently Deceased", "Delores Gleitman recently passed away at the age of 65. As you complete the survey, please draw upon your own personal beliefs about people who have passed away.");
+var fetus = new Character("fetus", "7-Week Human Fetus", "At 7 weeks, a human fetus is almost half an inch long - roughly the size of a raspberry.");
+var gerald = new Character("gerald-schiff_pvs", "Gerald Schiff, in Persistent Vegetative State", "Gerald Schiff has been in a persistent vegetative state (PVS) for the past six months. Although he has severe brain damage - Gerald does not appear to communicate with others or make purposeful movements - his basic bodily functions (such as breathing, sleeping, and circulation) are preserved.");
+var god = new Character("god", "God", "Many people believe that God is the creator of the universe and the ultimate source of knowledge, power, and love. However, please draw upon your own personal beliefs about God.");
+var greenfrog = new Character("green-frog", "Green Frog", "The Green Frog can be found throughout eastern North America. This classic 'pond frog' is medium-sized and green or bronze in color. Daily life includes seeking out permanent ponds or slow streams with plenty of vegetation.");
+var kismet = new Character("kismet_robot", "Kismet, Sociable Robot", "Kismet is part of a new class of 'sociable' robots that can engage people in natural interaction. To do this, Kismet perceives a variety of natural social signals from sound and sight, and delivers his own signals back to the human partner through gaze direction, facial expression, body posture, and vocal babbles.");
+var nicholas = new Character("nicholas-gannon_baby", "Nicholas Gannon, 5-Month-old", "Nicholas is a five-month-old baby.");
+var samantha = new Character("samantha-hill_girl", "Samantha Hill, 5-Year-old", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
+var sharon = new Character("sharon-harvey_woman", "Sharon Harvey, Advertising Executive", "Sharon Harvey, 38, works at an advertising agency in Chicago.");
+var toby = new Character("toby_chimp", "Toby, Wild Chimpanzee", "Toby is a two-year-old wild chimpanzee living at an outdoor laboratory in Uganda.");
+var todd = new Character("todd-billingsley_man", "Todd Billingsley, Accountant", "Todd Billingsley is a thirty-year-old accountant who lives in New York City.");
+var you = new Character("you", "You", "When you see the mirror, please consider how you, yourself, would compare with the other choice presented.");
 
 var characters = [charlie, delores, fetus, gerald, 
 				god, greenfrog, kismet, nicholas,
@@ -55,7 +56,6 @@ for (j = 0; j < characters.length; j++) {
 /* set up how to display experiment slides */
 
 var experiment = {
-	parts: ["consent", "instructions", "characters", "surveys", "stage", "results", "finished"],
 	condition: "none",
 	trials: pairs,
 	data: [], // where to store data
@@ -76,6 +76,8 @@ var experiment = {
 			this.data.rightImage = this.data.pair[sideBucket];
 			$("#image-left").attr("src", this.data.leftImage.imageSource);
 			$("#image-right").attr("src", this.data.rightImage.imageSource);
+			$("#text-left").text(this.data.leftImage.charTitle);
+			$("#text-right").text(this.data.rightImage.charTitle);
 			// var startTime = (new Date()).getTime(); // do I really want RT?
 		}
 	}
