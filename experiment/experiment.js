6d2be9d8a4cf4882821c4e85770bd12c0a8d6548
kgweisman <kweisman@stanford.edu>
2015-02-17 17:04:51 -0800

    style some stuff on results slide

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -35,8 +35,8 @@ addCharacter("gerald_schiff_pvs", "Gerald Schiff, in Persistent Vegetative State
 addCharacter("god", "God", "Many people believe that God is the creator of the universe and the ultimate source of knowledge, power, and love. However, please draw upon your own personal beliefs about God.");
 addCharacter("green_frog", "Green Frog", "The Green Frog can be found throughout eastern North America. This classic 'pond frog' is medium-sized and green or bronze in color. Daily life includes seeking out permanent ponds or slow streams with plenty of vegetation.");
 addCharacter("kismet_robot", "Kismet, Sociable Robot", "Kismet is part of a new class of 'sociable' robots that can engage people in natural interaction. To do this, Kismet perceives a variety of natural social signals from sound and sight, and delivers his own signals back to the human partner through gaze direction, facial expression, body posture, and vocal babbles.");
-addCharacter("nicholas_gannon_baby", "Nicholas Gannon, 5-Month-old", "Nicholas is a five-month-old baby.");
-addCharacter("samantha_hill_girl", "Samantha Hill, 5-Year-old", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
+addCharacter("nicholas_gannon_baby", "Nicholas Gannon, 5-Month-Old", "Nicholas is a five-month-old baby.");
+addCharacter("samantha_hill_girl", "Samantha Hill, 5-Year-Old", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
 addCharacter("sharon_harvey_woman", "Sharon Harvey, Advertising Executive", "Sharon Harvey, 38, works at an advertising agency in Chicago.");
 addCharacter("toby_chimp", "Toby, Wild Chimpanzee", "Toby is a two-year-old wild chimpanzee living at an outdoor laboratory in Uganda.");
 addCharacter("todd_billingsley_man", "Todd Billingsley, Accountant", "Todd Billingsley is a thirty-year-old accountant who lives in New York City.");
