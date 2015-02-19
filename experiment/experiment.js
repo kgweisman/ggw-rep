d0d97a78e67b599c1b406ff075241c5b686e7963
kgweisman <kweisman@stanford.edu>
2015-02-19 11:16:38 -0800

    preload images

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -1,18 +1,70 @@
-/* set up helper functions from long */
+/* set up helper functions */
 
+// define function to show a slide
 function showSlide(id) { 
 	// console.log(id); // show which slide is being presented
 	$(".slide").hide(); // hide all slides
 	$("#"+id).show(); // show selected slide
 };
 
-function randomInteger(n) { // get a random integer < n
+// define function to get a random integer < n
+function randomInteger(n) { 
 	return Math.floor(Math.random()*n); 
 }
 
-function randomElementNR(bucket) { // select random element of array without replacement
+// define function for random selection without replacement
+function randomElementNR(bucket) { 
 	var randomIndex = randomInteger(bucket.length);
 	return bucket.splice(randomIndex, 1)[0];
+}
+
+// define function for preloading images
+function preload(images, onLoadedOne, onLoadedAll) {
+	showSlide("preload");
+
+	var remainingImages = images.slice();
+	var finished = false;
+
+	// set delayInterval to 800 for testing to see that everything actually loads
+	// for real use, set to 0 
+	var loadDelayInterval = 800;
+
+	var worker = function() {
+	  if (remainingImages.length == 0) {
+	    if (!finished) {
+	      finished = true;
+	      setTimeout(onLoadedAll, loadDelayInterval);
+	    }
+	} else {
+
+	    var src = remainingImages.shift(); 
+	    
+	    var image = new Image();
+	    image.onload = function() {
+	        onLoadedOne();
+	        setTimeout(worker, loadDelayInterval);
+	    };
+	        image.src = src;
+		}
+	};
+
+	// load images 6 at a time
+	var concurrent = 13;
+	for(var i = 0; i < concurrent; i++) {
+		setTimeout(worker, 20 - i);
+		};
+	}
+
+// define a function that will get called every time one image is successfully loaded
+var numLoadedImages = 0;
+function onLoadedOne() {
+  numLoadedImages++;
+  // $("#num-loaded").text(numLoadedImages); 
+}
+
+// define a function that will get called once all images have been successfully loaded
+function onLoadedAll() {
+  showSlide("consent");
 }
 
 /* set up list of characters with titles, descriptions, and image sources */ 
@@ -433,6 +485,17 @@ var resultsSlide = {
 	}
 }
 
-/* show consent slide (which then advances through all remaining slides) */
+/* preload images */
+// declare the set of images we'd like to load
+var images = [];
+for (i = 0; i < Object.keys(characters).length; i++) {
+	images[i] = characters[Object.keys(characters)[i]].imageSource;
+}
 
-showSlide("consent");
+preload(images,
+        onLoadedOne,
+        onLoadedAll);
+
+//  show consent slide (which then advances through all remaining slides) 
+
+// showSlide("consent");
