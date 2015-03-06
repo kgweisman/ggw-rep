0f5241350145c100b3003c1b244326d1739abd53
kgweisman <kweisman@stanford.edu>
2015-03-05 23:04:07 -0800

    conduct original factor analysis

--- analysis/ggw-rep_analysis1.R
+++ analysis/ggw-rep_analysis1.R
@@ -1,10 +1,11 @@
+# --- PRELIMINARIES -----------------------------------------------------------
+
 # libraries
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(lme4)
-library(jsonlite)
-library(stats)
+library(psych)
 
 # clear environment
 rm(list=ls())
@@ -13,6 +14,8 @@ rm(list=ls())
 d = read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-rep/ggw-rep/data/pilot-b_01_charmeans.csv")[-1] # get rid of column of obs numbers
 
 glimpse(d)
+
+# --- DATA FORMATTING ---------------------------------------------------------
 
 # make table of character means by mental capacity
 charmeans = d %>%
@@ -27,3 +30,21 @@ charmeans_table = charmeans %>%
   spread(condition, mean)
   
 View(charmeans_table)
+
+
+# --- FACTOR ANALYSIS #1: ORIGINAL GGW2007 _-----------------------------------
+# Extract 2 PCs (unrotated)
+pca1 = principal(charmeans_table[2:6], nfactors = 2, rotate = "none"); pca1
+
+# Extract 2 PCs (rotated)
+pca2 = principal(charmeans_table[2:6], nfactors = 2, rotate = "varimax"); pca2
+
+# Plot PCs against each other for both solutions
+par(mfrow=c(1,2))
+variables = names(charmeans_table[2:6])
+plot(pca1$loadings, type='n')
+text(pca1$loadings, labels=variables, cex=.9)
+plot(pca2$loadings, type='n')
+text(pca2$loadings, labels=variables, cex=.9)
+
+
