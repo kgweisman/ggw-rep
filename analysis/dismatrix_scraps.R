dd109f04e860cb960c73ae0669089e1008f7f8c4
kgweisman <kweisman@stanford.edu>
2015-03-06 02:21:43 -0800

    delete scraps for creating dissimilarity matrix with alternative (inferior) method

--- analysis/dismatrix_scraps.R
+++
@@ -1,9 +0,0 @@
-# compute pairwise dissimilarities
-dismatrix <- dd %>%
-  select(subid, condition, leftCharacter, rightCharacter, responseNum) %>%
-  group_by(rightCharacter, leftCharacter) %>%
-  mutate(dist = abs(responseNum)) %>% # use absolute values of comparison scores to get distance
-  summarise(mean = mean(dist, na.rm = TRUE)) %>%
-  spread(rightCharacter, mean)
-
-View(dismatrix)
