1dbfe8e6d527ce4857e1f3dc84a9461725fac279
kgweisman <kweisman@stanford.edu>
2015-03-05 22:53:43 -0800

    format wideform character means by condition table

--- analysis/ggw-rep_analysis1.R
+++ analysis/ggw-rep_analysis1.R
@@ -10,6 +10,20 @@ library(stats)
 rm(list=ls())
 
 # read in anonymized data
-d = read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-rep/ggw-rep/data/pilot-b_01_data_anonymized.csv")[-1] # get rid of column of obs numbers
+d = read.csv("/Users/kweisman/Documents/Research (Stanford)/Projects/GGW-rep/ggw-rep/data/pilot-b_01_charmeans.csv")[-1] # get rid of column of obs numbers
 
 glimpse(d)
+
+# make table of character means by mental capacity
+charmeans = d %>%
+  gather(character, response, -subid, -condition) %>%
+  group_by(condition, character) %>%
+  summarise(mean = mean(response, na.rm = T))
+
+glimpse(charmeans)
+
+# format into wideform
+charmeans_table = charmeans %>%
+  spread(condition, mean)
+  
+View(charmeans_table)
