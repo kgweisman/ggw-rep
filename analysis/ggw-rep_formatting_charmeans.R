449f092a5fe0d02d5f552a984a122f1290485f26
kgweisman <kweisman@stanford.edu>
2015-03-09 10:16:47 -0700

    rename 'Emotion Recognition' without space

--- analysis/ggw-rep_formatting_charmeans.R
+++ analysis/ggw-rep_formatting_charmeans.R
@@ -84,6 +84,9 @@ glimpse(d.raw)
 # clean up variables
 d_tidy = d.raw %>%
   mutate(subid = factor(subid),
+         condition = ifelse(condition == "Emotion Recognition", 
+                            "EmotionRecognition",
+                            condition),
          condition = factor(condition),
          gender = factor(gender),
          age = as.numeric(age),
