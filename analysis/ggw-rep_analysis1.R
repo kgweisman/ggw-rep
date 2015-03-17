bf57ac882f93a7208d3a3e2f71e37fad80cbd558
kgweisman <kweisman@stanford.edu>
2015-03-17 11:08:55 -0700

    get rid of some baggage around multiplot function

--- analysis/ggw-rep_analysis1.R
+++ analysis/ggw-rep_analysis1.R
@@ -17,18 +17,7 @@ rm(list=ls())
 dev.off()
 
 # define multiplot function
-# source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
-
-# Multiple plot function
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
+# source: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   require(grid)
   
